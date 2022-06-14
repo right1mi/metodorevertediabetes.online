9b53671366001560f333babb07c14cd03597a186
right1mi <103401473+right1mi@users.noreply.github.com>
2022-06-14 17:47:16 -0300

    Add files via upload

---
+++ assets/js/delay.js
@@ -0,0 +1,13 @@
+const itemDelay = document.querySelectorAll("[data-delay]");
+var temp =19.50;
+var tempConv = temp * 60000;
+
+function hide() {
+    itemDelay.forEach(item => {
+        item.style.display = "block";
+    });
+}
+
+setTimeout(() => {
+    hide();
+}, tempConv)
